ac1549640d59d12fe1ba86d491e883594f0e0fde
Eden Mesfun <emesfun@gmail.com>
2018-01-30 20:01:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ test_range = [-1, 0, 1, 4, 40, 1763, 101013, 2, 3, 11, 105557]
 def prime?(num)
   end_num = num - 1
   range = (2..end_num).to_a
-    if num <= 0 || num == 1 || num.even?
+    if num <= 0 || num == 1
       return false
     end
   range.all? do |numbers|
