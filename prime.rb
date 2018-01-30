8808933748d2ce80d86cd2427c77126aeb6ec0de
Eden Mesfun <emesfun@gmail.com>
2018-01-30 19:47:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ require 'pry'
 test_range = [-1, 0, 1, 4, 40, 1763, 101013, 2, 3, 11, 105557]
 
 def prime?(num)
-  var = [2..end_num]
+  range = (2..end_num)
   end_num = num -1
 
     if num != 2 && num.even?
